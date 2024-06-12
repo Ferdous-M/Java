6567c89c24680b42403c9f43f5112d05814d501a
Ferdous-M <137258398+Ferdous-M@users.noreply.github.com>
2024-06-12 12:10:32 +0600

    Create 01.java

---
+++ 01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    String s;
+	    Scanner myObj = new Scanner(System.in); 
+	    s=myObj.nextLine();
+		System.out.println("Hello, "+s);
+	}
+}
